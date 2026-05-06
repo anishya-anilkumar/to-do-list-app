fcb9035283ef3c1eaca3d138701a9ad0d7c9285a
Anishya Anilkumar <anishyaanilkumar@gmail.com>
2026-05-06 17:52:13 +0000

    added pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "TaskFlow To-Do List",
+        short_name: "TaskFlow",
+        description: "Stay organized and productive with timed tasks.",
+        theme_color: "#f69dbc",
+        background_color: "#a8023c",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 })
